55d0d232206d249a5848ab46cf52120c743cb78b
levimendyk <levi3743@gmail.com>
2022-07-27 20:35:20 -0600

    linked app.js to html and added psuedo coding to Javascript

---
+++ assets/js/app.js
@@ -0,0 +1,15 @@
+console.log("test");
+// VARIABLES
+// var the API for Weather
+var apiCall =
+  "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}";
+
+// FUNCTIONS
+// Create function to search a City
+// Create function to display todays weather information in the "todays-weather" class ((Include City & Date & Icon.img; Temp; Wind; Humidity; UV Index(changes to Green, Yellow, or Red)))
+// Create function to display the next five days weather information in the "five-day-forecast" container ((Include Date; Icon.img; Temp; Wind; Humidity))
+// Create function to log last search (searches)
+// Create function to display the search histories
+// Create function to click on search histories to pull up the current and five day forecast
+
+// LOGIC
